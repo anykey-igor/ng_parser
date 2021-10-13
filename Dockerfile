2ed9ab6ae4b56ddec4526e931481c597a67d58ee
ia <ia@ucode.world>
2021-10-13 20:04:13 +0300

    Add git clone/push add sort mode and fix some error

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,18 @@ ARG PYTHON_VERSION=3.8
 
 FROM python:${PYTHON_VERSION}
 
+ENV GH_TOKEN=your GH token
+
+ENV REPO_URL= Your repository for LOG file /username/reponame.git
+
 RUN mkdir /ng_parser
+
 WORKDIR /ng_parser
 ADD requirements.txt /ng_parser/
 RUN pip install -r requirements.txt
-ADD . /ng_parser/
+ADD access.log /ng_parser/
+RUN rm requirements.txt
 COPY ng_parser.py /ng_parser/
+COPY .gitconfig /root
 
 ENTRYPOINT ["python", "ng_parser.py"]
--- ng_parser.py
+++ ng_parser.py
@@ -8,7 +8,9 @@ import pytz
 import csv
 import json
 import collections
+import git
 from git import Repo
+from subprocess import Popen, PIPE, STDOUT
 
 
 tz = pytz.timezone('Europe/Kiev')
@@ -16,13 +18,13 @@ tz = pytz.timezone('Europe/Kiev')
 #filename = './access.log'
 
 lineformat = re.compile(
-    r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""",
+    r"""(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<timestamp>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] (["]((?P<method>GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""",
     re.IGNORECASE)
 
 
 def write_csv(data, name='outfile.csv'):
     with open(name, '+w', newline='') as csvfile:
-        fieldnames = ["ipaddress", "dateandtime", "url", "statuscode", "bytessent", "refferer", "useragent"]
+        fieldnames = ["ip", "timestamp", "method", "url", "statuscode", "bytessent", "refferer", "useragent"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for d in data:
@@ -40,11 +42,11 @@ dump_func = {
 }
 
 sort_mode = {
-    'ip': 'ipaddress',
-    'date': 'dateandtime',
+    'ip': 'ip',
+    'time': 'timestamp',
     'method': 'method',
     'url': 'url',
-    'status': 'status',
+    'status': 'statuscode',
     'byte': 'bytessent',
     'ref': 'refferer',
     'user': 'useragent'
@@ -66,19 +68,24 @@ def log_parser(filename=str, sort=False, filter=False, time=False):
     for l in list:
         data = re.search(lineformat, l)
         if data:
-            if data:
-                datadict = data.groupdict()
-                ip = datadict["ipaddress"]
-                datetimeobj = datetime.strptime(datadict["dateandtime"],
-                                                "%d/%b/%Y:%H:%M:%S %z")  # Converting string to datetime obj
-                url = datadict["url"]
-                bytessent = datadict["bytessent"]
-                referrer = datadict["refferer"]
-                useragent = datadict["useragent"]
-                status = datadict["statuscode"]
-                method = data.group(6)
+            datadict = data.groupdict()
+            ip = datadict["ip"]
+            datetimeobj = datetime.strptime(datadict["timestamp"],
+                                            "%d/%b/%Y:%H:%M:%S %z")  # Converting string to datetime obj
+            method = datadict["method"]
+            url = datadict["url"]
+            bytessent = int(datadict["bytessent"])
+            referrer = datadict["refferer"]
+            useragent = datadict["useragent"]
+            status = datadict["statuscode"]
         ng_log.append(datadict)
     ng_result = ng_log
+
+    for i in ng_result: #конвертация строки в int
+        i['statuscode'] = int(i['statuscode'])
+        i['bytessent'] = int(i['bytessent'])
+        #print(type(i['statuscode']))
+
     if sort in sort_mode:
         ng_sorted = sorted(ng_log, key=lambda k: k[sort_mode[sort]])
         ng_result = ng_sorted
@@ -103,7 +110,7 @@ def main():
                         help='Select CSV or JSON format file')
 
     parser.add_argument('-s', '--sort',
-                        choices=['ip', 'url', 'statuscode', 'agent'],
+                        choices=['ip', 'time', 'method', 'byte', 'url', 'status', 'agent'],
                         help='Select sort mode report')
 
     parser.add_argument('-t', '--time',
@@ -113,7 +120,7 @@ def main():
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     if args.file:
-        result = log_parser(args.file)
+        result = log_parser(args.file, args.sort)
 
     if args.output in dump_func:
         dump_func[args.output](result)
@@ -121,5 +128,56 @@ def main():
         print('For save results, please use argument --outuput select csv or json')
 
 
+def check_dir():
+    dir_in_curdir = filter(os.path.isdir, os.listdir(os.curdir))
+    dir_list = list(dir_in_curdir)
+    repo_url = os.environ['REPO_URL']
+    git_dir = {}
+
+    for i in dir_list:
+        if is_git_repo(i):
+            git_dir.update({i:True})
+
+    repo_name = repo_url.split('/')[-1][:-4]
+    if repo_name in git_dir:
+        os.chdir(repo_name)
+        repo = git.Git(repo_name)
+        p = repo.fetch()
+        print(f'We have Git repo')
+
+    else:
+        repo = git.Git(repo_name)
+        git_clone(repo_name)
+        os.chdir(repo_name)
+        print('Clon repo')
+
+
+
+def git_clone(path):
+    repo = git.Git(path)
+    gh_token = os.environ['GH_TOKEN']
+    repo_url = os.environ['REPO_URL']
+    url_clone = "https://" + gh_token + ":x-oauth-basic@github.com" + repo_url
+    repo.clone(url_clone)
+
+
+def is_git_repo(path):
+    try:
+        _ = git.Repo(path).git_dir
+        return True
+    except git.exc.InvalidGitRepositoryError:
+        return False
+
+def git_push(path):
+    repo = git.Git(path)
+    p = repo.add('.')
+    p = repo.commit(m='my commit message+')
+    p = repo.push()
+
 if __name__ == '__main__':
+
+    my_dir = os.path.abspath(os.curdir)
+    check_dir()
+    my_dir1 = os.path.abspath(os.curdir)
     main()
+    git_push(my_dir1)
